49e1cb01ccf422177cb88d7bf0a8e0b17394fafc
Bukan YOUTUBER <vradita29@gmail.com>
2024-05-14 22:02:22 +0700

    add user 60%

--- src/components/list/chatlist/addUser/AddUser.jsx
+++ src/components/list/chatlist/addUser/AddUser.jsx
@@ -1,19 +1,67 @@
+import { collection, doc, getDoc, getDocs, query, serverTimestamp, setDoc, where } from 'firebase/firestore'
 import './addUser.css'
+import { db } from '../../../../lib/firebase'
+import { useState } from 'react'
 
 const AddUser = () => {
+
+    const [user, setUser] = useState(null)
+
+    const handleSearch = async (e) => {
+        e.preventDefault()
+
+        const formData = new FormData(e.target)
+        const username = formData.get("username")
+
+        try {
+            const userRef = collection(db, "users");
+            const q = query(userRef, where("username", "==", username));
+
+            const querySnapShot = await getDocs(q)
+
+            if (!querySnapShot.empty) {
+                setUser(querySnapShot.docs[0].data())
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleAdd = async () => {
+        const chatRef = collection(db, "chats");
+        const userChatRef = collection(db, "userchats");
+
+        try {
+            const newChatRef = doc(chatRef);
+
+            await setDoc(newChatRef, {
+                createdAt: serverTimestamp(),
+                messages: []
+            });
+
+
+            console.log("New chat created:", newChatRef.id);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+
     return (
         <div className='addUser'>
-            <form >
+            <form onSubmit={handleSearch}>
                 <input type="text" placeholder='Username' name='username' />
                 <button>Search</button>
             </form>
-            <div className="user">
+            {user && <div className="user">
                 <div className="detail">
-                    <img src="./avatar.png" alt="avatar" />
-                    <span>Pakde</span>
+                    <img src={user.avatar || "./avatar.png"} alt="avatar" />
+                    <span>{user.username}</span>
                 </div>
-                <button>Add User</button>
+                <button onClick={handleAdd}>Add User</button>
             </div>
+            }
         </div>
     )
 }
